9d3be27541cd2b942dd354541cee94107f7a48ac
Stephen Davis <stephendavis2016@gmail.com>
2021-12-28 20:03:00 +0000

    Successful connection of user input to api call

--- Visualisation/visualisation-app/src/SearchBar.js
+++ Visualisation/visualisation-app/src/SearchBar.js
@@ -1,22 +1,53 @@
+import { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./SearchBar.css"
 
-function SearchBar() {
-    return (
-        <div>
-            <form action="/" method="get">
-                <label htmlFor="header-search">
-                    <span className="visually-hidden">Search username</span>
-                </label>
-                <input
-                    type="text"
-                    id="header-search"
-                    placeholder="Search username"
-                    name="s" 
-                />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    );
+class SearchBar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {value: '', testFlask: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event) {
+        // console.log(this.state.value);
+        event.preventDefault();
+        
+        fetch("/"+this.state.value).then(
+            res => res.json()
+            ).then(
+              data => {
+                this.setState({testFlask: data.user});
+                console.log("CHECKING VALUE: "+this.state.testFlask)
+          });
+    }    
+
+    render() {
+        return (
+            <div>
+                <form action="/" method="get" onSubmit={this.handleSubmit}>
+                    <label htmlFor="header-search">
+                        <span className="visually-hidden">Search username</span>
+                    </label>
+                    <input
+                        type="text"
+                        id="header-search"
+                        placeholder="Search username"
+                        name="s"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default SearchBar;
