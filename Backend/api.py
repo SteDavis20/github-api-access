b003f704726862bf0c56a1baadd70c2542b23965
Stephen Davis <stephendavis2016@gmail.com>
2021-12-29 00:40:39 +0000

    Writing api calls which use imported functions to get follower info and accumulated ratio

--- Backend/api.py
+++ Backend/api.py
@@ -5,17 +5,33 @@ app = Flask(__name__)
 
 pd = PopulateData()
 
+# cannot return list in app.route methods, must convert to json using json.dumps(list)
+
 # localhost:5000/testing
 @app.route("/testing")
 def get_test_values():
     return {"test": ["Test1", "Test2"]}
 
-@app.route("/<username>")
-def get_front_end_input(username):
+@app.route("/<username>/info")
+def get_user_info(username):
     user = pd.getGithubUser(username)
     dictionary = pd.extractDataIntoDictionary(user)
     dictionary = pd.removeNullDataInDictionary(dictionary)
     return dictionary
+
+@app.route("/<username>/followerInfo")
+def get_follower_info(username):
+    user = pd.getGithubUser(username)
+    list = pd.getFollowerInfo(user, [])
+    jsonString = json.dumps(list)    
+    return jsonString
+
+@app.route("/<username>/accumulatedRatio")
+def get_accumulated_ratio(username):
+    user = pd.getGithubUser(username)
+    list = pd.countFollowersOfFollowers(user, [])
+    jsonString = json.dumps(list)    
+    return jsonString
 
 # api is run on localhost:5000
 if __name__ == "__main__":
